ef9086d86fafbcab611fb5002d44098ecb969bef
Justin Haghighi <jhaghighi@nevada.unr.edu>
2020-04-09 03:00:57 +0000

    Please work

---
+++ BMNetGenGUI.py
@@ -0,0 +1,50 @@
+##############################################################################
+# Name: Justin A. Haghighi
+# Date: 2020-04-08
+# Course: CS 426.1001
+# Instructors: Devrin Lee, MS, and Sergiu Dascalu, PhD
+# Team: 30
+##############################################################################
+
+from BMNetGen import generateTopo
+import threading
+
+def generateNetworkBody():
+    # Justin's rough HTML body is below
+    body = """
+    <br>
+        <button id="networkStartButton" data-xdh-onevent="networkStart" {}>
+            Generate network
+        </button>
+        </button>
+        <button id="networkBenchButton" data-xdh-onevent="networkBench" {}>
+            Network benchmark
+        </button></button>
+        <button id="showGraphButton" data-xdh-onevent="showGraph" {}>
+            Show graph
+        </button>
+    <br>
+    """
+    return body
+
+def acNetworkStart(dom):
+    BMNetGen.generateTopo()
+
+def acNetworkBench(dom):
+    pass
+
+def acShowGraph(dom):
+    pass
+
+networkCallbacks = {
+    "networkStart": acNetworkStart,
+    "networkBench": acNetworkBench,
+    "showGraphButton": acShowGraph,
+}
+
+
+
+
+
+
+
