1730d0c71fe203332f346db06320e538c84a36d6
J-Andrieu <jandrieu@nevada.unr.edu>
2020-04-25 13:47:54 -0700

    The docker building works, and the gui runs in it, yay

--- container/ENTRYPOINT.sh
+++ container/ENTRYPOINT.sh
@@ -3,6 +3,8 @@
 service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
-bash
-
 service openvswitch-switch stop
+
+bash -c "python3 /root/CS-425-PROTOTYPE/gui.py"
+
+exit
--- fuzzerTests.py
+++ fuzzerTests.py
@@ -1,7 +1,9 @@
+import pytest
 from EnsembleFuzzer import Fuzzer
+from scapy.all import *
 
 #Test reproduceability
-def reproduceabilityTest():
+def test_reproduceable_fuzz():
     fuzzR1 = Fuzzer(["radamsa"], 1)
     fuzzR2 = Fuzzer(["radamsa"], 1)
     fuzzB1 = Fuzzer(["blab"], 42)
@@ -14,24 +16,33 @@ def reproduceabilityTest():
         packetR1, garbageVal = states["radamsa"]
         states = fuzzR2.Fuzz()
         packetR2, garbageVal = states["radamsa"]
+        assert packetR1 == packetR2
         if packetR1 != packetR2:
             success = False
             print("Radamsa fuzzer failed reproduceability test!")
-    
+
         states = fuzzB1.Fuzz()
         packetB1, garbageVal = states["blab"]
         states = fuzzB2.Fuzz()
         packetB2, garbageVal = states["blab"]
+        assert packetB1 == packetB2
         if packetB1 != packetB2:
             success = False
             print("Blab fuzzer failed reproduceability test!")
-        
+
         if success == False:
             break
 
     return success
 
 #Test packet structure
+def test_openflow_packet_structure():
+    pass
+
+def test_tcp_packet_structure():
+    pass
 
 if __name__ == "__main__":
-    reproduceabilityTest()
+    test_reproduceable_fuzz()
+    test_openflow_packet_structure()
+    test_tcp_packet_structure()
