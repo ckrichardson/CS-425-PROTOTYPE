fac3b91c9a2bcd1f61f64262c90a9ea23c6d1afa
J-Andrieu <jandrieu@nevada.unr.edu>
2020-04-25 14:02:07 -0700

    updated entrypoint...

--- container/ENTRYPOINT.sh
+++ container/ENTRYPOINT.sh
@@ -3,8 +3,6 @@
 service openvswitch-switch start
 ovs-vsctl set-manager ptcp:6640
 
-service openvswitch-switch stop
-
 bash -c "python3 /root/CS-425-PROTOTYPE/gui.py"
 
-exit
+service openvswitch-switch stop
