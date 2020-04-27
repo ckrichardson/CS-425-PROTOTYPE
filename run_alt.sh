534e9147170bba34b103066e3ce13181a57ae0e0
Justin Haghighi <jhaghighi@A1502.haghighi.me>
2020-04-27 14:46:00 -0700

    Add run_alt.sh to the repo. This replaces run.sh once tested.

---
+++ run_alt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo docker run --rm --privileged -it \
+   --user=root \
+   --env="DISPLAY" \
+   --workdir=/app \
+   --volume="$PWD":/app \
+   --volume="/etc/group:/etc/group:ro" \
+   --volume="/etc/passwd:/etc/passwd:ro" \
+   --volume="/etc/shadow:/etc/shadow:ro" \
+   --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+   --volume="/lib/modules:/lib/modules" \
+   sdn-toolkit
