ad8e23919e05ef7418746745b75558a674a508f7
ckrichardson <clifford_richarsdon@nevada.unr.edu>
2020-04-28 06:03:17 -0700

    more network things

--- binaryAnalysis/find_network_sigs.py
+++ binaryAnalysis/find_network_sigs.py
@@ -2,10 +2,10 @@ import os
 
 
 def find_syscalls(path):
-    if not os.isfile(path):
+    if not os.path.isfile(path):
         return "bad path"
 
-    list_of_sys[] = []
+    list_of_sys = []
 
     with open(path, "r") as inputfile:
         for line in inputfile:
@@ -14,17 +14,26 @@ def find_syscalls(path):
                 if "Sys" in element and element not in list_of_sys:
                     list_of_sys.append(element)
 
+    return list_of_sys
+
 def find_network_syscalls(path):
-    if not os.isfile(path):
+    if not os.path.isfile(path):
         return "bad path"
 
-    list_of_sys[] = []
+    if ".analytics" not in path:
+        return "bad file"
 
-    with open(path,"r" as inputfile:
+    list_of_sys = []
+
+    with open(path,"r") as inputfile:
             for line in inputfile:
-            fields =  line.split(" ")
+                fields =  line.split(" ")
             for element in fields:
                 if "Sys" in element and element not in list_of_sys:
                     list_of_sys.append(element)
+    return
 
+if __name__ == "__main__":
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    print(find_syscalls(file_dir + "/100.analytics"))
 
