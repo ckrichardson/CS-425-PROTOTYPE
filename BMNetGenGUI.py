8dc3c62bfa7957e6b05f35db4def0806dbb905b8
Justin Hagighi <jhaghighi@nevada.unr.edu>
2020-05-01 14:30:22 -0700

    GUI debugging

--- BMNetGenGUI.py
+++ BMNetGenGUI.py
@@ -94,7 +94,7 @@ def acSendGraphEmail(dom):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, tput_email, text)
+        server.sendmail(sender_email, receiver_email, text)
     print("*** Email sent")
 
 networkCallbacks = {
